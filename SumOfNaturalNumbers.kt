a57a87b97eb7bb12d22520910c8f159be3b9e336
Vikash Singh Bhadoriya <codingwithvikash@gmail.com>
2021-07-05 12:45:04 +0530

    added SumOfNaturalNumbers.kt
    which will calculate the sum of natural numbers using a for loop

---
+++ SumOfNaturalNumbers.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val num = 12
+    var sum = 0
+
+    for (i in 1..num) {
+        // sum = sum+i;
+        sum += i
+    }
+
+    println("Sum of $num natural number = $sum")
+}
